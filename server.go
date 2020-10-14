7b6f30d5125ed438c4e718cb28756703be3e5366
Patrick Furtak <patrick@patrickfurtak.com>
2020-10-14 16:35:38 -0400

    Wrote custom logger middleware, writes to logfile gin.log

--- server.go
+++ server.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"io"
+	"os"
+
 	"github.com/gin-gonic/gin"
 	"github.com/pfurtak/learning-gin/controller"
+	"github.com/pfurtak/learning-gin/middleware"
 	"github.com/pfurtak/learning-gin/service"
 )
 
@@ -11,8 +15,18 @@ var (
 	videoController controller.VideoController = controller.New(videoService)
 )
 
+func logOutput() {
+	f, _ := os.Create("gin.log")
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+}
+
 func main() {
-	server := gin.Default()
+
+	logOutput()
+
+	server := gin.New()
+
+	server.Use(gin.Recovery(), middleware.Logger())
 
 	server.GET("/videos", func(ctx *gin.Context) {
 		ctx.JSON(200, videoController.FindAll())
